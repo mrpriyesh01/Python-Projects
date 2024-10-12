034ca5f94f67ea2831db201173828fab496c0e9a
Priyesh Singh <mrpriyesh037@gmail.com>
2024-10-12 12:43:40 +0530

    8 - Library Borrowing Habits.sql

---
+++ 8 - Library Borrowing Habits.sql
@@ -0,0 +1,32 @@
+Imagine you're working for a library and you're tasked with generating a report on the borrowing habits of patrons. You have two tables in your database: Books and Borrowers.
+Write an SQL to display the name of each borrower along with a comma-separated list of the books 
+they have borrowed in alphabetical order, display the output in ascending order of Borrower Name.
+Tables: Books
++-------------+-------------+
+| COLUMN_NAME | DATA_TYPE   |
++-------------+-------------+
+| BookID      | int         |
+| BookName    | varchar(30) |
+| Genre       | varchar(20) |
++-------------+-------------+
+Tables: Borrowers
++--------------+-------------+
+| COLUMN_NAME  | DATA_TYPE   |
++--------------+-------------+
+| BorrowerID   | int         |
+| BorrowerName | varchar(10) |
+| BookID       | int         |
+
+  
+code=
+SELECT 
+  b.BorrowerName, 
+  GROUP_CONCAT(book.BookName ORDER BY book.BookName ASC SEPARATOR ', ') AS BorrowedBooks
+FROM 
+  Books book
+JOIN 
+  Borrowers b
+ON 
+  b.BookID = book.BookID
+GROUP BY 
+  b.BorrowerName;
